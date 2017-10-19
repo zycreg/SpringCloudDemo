adbe2e5e9ce1043faba628a4543586a723e2733a
zhangyuancheng <zhangyuancheng@fulu.com>
2017-10-19 15:29:21 +0800

    commit by zyc on 2017/09/21

---
+++ service-zuul/src/main/java/com/zyc/MyFilter.java
@@ -0,0 +1,53 @@
+package com.zyc;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by zhangyuancheng on 2017/10/19.
+ */
+@Component
+public class MyFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if(accessToken == null) {
+            log.warn("token is empty");
+            context.setSendZuulResponse(false);
+            context.setResponseStatusCode(401);
+            try {
+                context.getResponse().getWriter().write("token is empty");
+            }catch (Exception e){}
+
+            return null;
+        }
+        log.info("ok");
+        return null;
+    }
+}
